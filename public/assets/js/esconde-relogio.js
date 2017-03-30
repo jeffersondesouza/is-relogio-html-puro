c32866fb8f8dd9d36f83930e26d265c38902c8ff
Jefferson <jeffersondesouzacordel@.com>
2017-03-30 15:36:04 -0300

    s

---
+++ public/assets/js/esconde-relogio.js
@@ -0,0 +1,2 @@
+let relogioOn=!0;let relogio=document.querySelector('.relogio');let botaoEsconde=document.querySelector('.btn-esconde-relogio');let botaoMostra=document.querySelector('.btn-mostra-relogio');botaoEsconde.addEventListener('click',()=>{relogio.classList.add('elemento-escondido');botaoMostra.classList.remove('elemento-escondido');botaoEsconde.classList.add('elemento-escondido')})
+botaoMostra.addEventListener('click',()=>{relogio.classList.remove('elemento-escondido');botaoEsconde.classList.remove('elemento-escondido');botaoMostra.classList.add('elemento-escondido')})
